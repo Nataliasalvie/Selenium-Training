65545c310d914f76fdd292f9835c4fb676d84317
Nataliasalvie <natalia.turcanu@mail.ru>
2024-02-11 17:21:42 +0200

    Homework - css selectors 8 february

--- src/main/java/md/natalia/main/Main.java
+++ src/main/java/md/natalia/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		ChromeDriver driverChrome = new ChromeDriver();
+		//ChromeDriver driverChrome = new ChromeDriver();
 		
 		/** Open darwin.md */
 		//driverChrome.get("https://darwin.md/");
@@ -21,21 +21,229 @@ public class Main {
 		
 		/** Find elements by ID Locator */
 		
-		driverChrome.get("https://www.enter.online/");
-		driverChrome.manage().window().maximize();
-		driverChrome.findElement(By.id("search")).click();
-        driverChrome.findElement(By.id("search")).sendKeys("aparat");
-		driverChrome.findElement(By.cssSelector("#search-icon > svg")).click();
+		//driverChrome.get("https://www.enter.online/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.id("search")).click();
+       // driverChrome.findElement(By.id("search")).sendKeys("aparat");
+		//driverChrome.findElement(By.cssSelector("#search-icon > svg")).click();
 		
 		
 		/** Find elements by Class Name Locator */
 		
-		driverChrome.findElement(By.className("block-header")).click();
-		driverChrome.findElement(By.className("content")).click();
-		driverChrome.findElement(By.className("footer")).click();
+		//driverChrome.findElement(By.className("block-header")).click();
+		//driverChrome.findElement(By.className("content")).click();
+		//driverChrome.findElement(By.className("footer")).click();
 		
-        
+		
+		//XPath 
+		//TagName[@atribute='value']
+		//driverChrome.get("https://www.samsung.com/uk/");
+		//driverChrome.manage().window().maximize();
+				//driverChrome.findElement(By.xpath("//*[@id=\"truste-consent-button\"]")).click();
+				//driverChrome.findElement(By.xpath("//*[@id=\"component-id\"]/div[1]/div[5]/ul[1]/li[6]/button")).click();
+				//driverChrome.findElement(By.xpath("/html/body/div[1]/nav/div[1]/div[5]/ul[1]/li[6]/div/div[1]/ul/li[3]/a")).click();
+				
+		//XPath Relativ	
+		
+		//driverChrome.get("https://apple.com");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.xpath("//span[@Class='globalnav-link-text-container']")).click();
+				
+		//XPath Absolute
+		
+		//driverChrome.get("https://apple.com");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.xpath("/html/body/div/nav/div/ul/li/div/div/div/ul/li/a/span")).click();		
+		
+		//XPath starts-with
+		//formula: tagName[starts-with(@atribute,'value')]
+		
+		//driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+	    //driverChrome.findElement(By.xpath("//li[starts-with(@id,'lev')]")).click();
+	    
+		
+	    
+		//XPath ends-with trebuie de corectat
+	    //formula: tagName[ends-with(@atribute,'value')]
+	    //driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+		
+	    //driverChrome.findElement(By.xpath("//li[ends-with(@id,'-1-0')]")).click();
+	    
+	    
+	    //XPath contains-with
+	    //formula: tagName[ends-with(@atribute,'value')]
+	    //driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+		//diver  id
+	    //driverChrome.findElement(By.xpath("//li[contains-with(@id,'-1-0')]")).click();	
+		
+		//XPath text
+		
+		
+		//formula: tagName[starts-with(@atribute,'value')]
+		
+		//driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+	    //driverChrome.findElement(By.xpath("//li[starts-with(@id,'lev')]")).click();
+	    
+		//XPATH  text()
+//		//tagname[text()='actual text']
+		
+		//driverChrome.get("www.darwin.md");
+	    //driverChrome.manage().window().maximize();
+	    //driverChrome.findElement(By.xpath("[//span[text()='toate categoriile']")).click();
+		
+		
+	 // XPath AND comulativ
+	 	// XPATH=//tagname[@Attribute=’Value’ and @Attribute=’Value’]
+	    
+	    
+	    
+	 // XPath OR 
+	 		// XPATH=//tagname[@Attribute=’Value’ or @Attribute=’Value’] (edited) 
+		//driverChrome.get("https://www.darwin.md");
+ 		//driverChrome.manage().window().maximize();
+ 		//driverChrome.findElement(By.xpath("//img[@alt='Ceasuri și brățări' and @width='104']")).click();
+    
+		
+	    
+	 // XPATH=//tagname[@Attribute=’Value’ or @Attribute=’Value’]
+		
+	 		
+	 		//driverChrome.get("https://www.darwin.md");
+	 		//driverChrome.manage().window().maximize();
+	 		//driverChrome.findElement(By.xpath("//img[@alt='Ceasuri și brățări' or @width='104']")).click();
+	    
+		//8.02.2024
+		//Css selector Id
+		//tagName[AttributeName=’AttributeValue’] 
+		
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		
+		
+		//tagName#elementID
+		//ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button#onetrust-accept-btn-handler")).click();
+		
+		
+		//#elementID
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.lamborghini.com/en-en");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("#onetrust-accept-btn-handler")).click();
+				
+		
+		//Css Class
+		//tagName.Attribute_Value (valoarea clasei)
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("a.css-1gpccxq")).click();
+		
+		
+		
+		//Css alte atribute
+		//tagName[AttributeName=’AttributeValue’] formula generala
+				
+	    //ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-testid='search-input']")).click();
+		
+		//exercitiu
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-testid='search-input']")).sendKeys("cosmetica");
+		
+		
+		//tagName[AttributeName=’AttributeValue’][ AttributeName=’AttributeValue’]
+		
+				
+				//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-cy='search-bar-input'][placeholder='Ce anume cauți?']")).click();
+		
+		//Dupa prefix, dupa sufix]
+				//input[name^=’fixed_d’]
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-cy^='search-']")).click();
+		
+		//dupa suffix]
+		//suffix tagname[name$=’d_fixed’]
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-cy$='-input']")).click();
+		
+		//css matching substring 
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("input[data-cy*='-bar-']")).click()
+		
+		//Dupa copil
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("div[class='css-1rwzo2t']>a[class='css-1gpccxq']")).click();
+		
+		//
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("div[class='css-1rwzo2t']>a[class='css-1gpccxq']")).click();
+		
+		
+		
+	    
+	  //Tema pentru acasa Locator CSS
+		
+		
+		ChromeDriver driverChrome = new ChromeDriver();
+		driverChrome.get("https://www.olx.ro/");
+		driverChrome.manage().window().maximize();
+		driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		
+			//1 ID, 
+			//2 Class,
+			//4 Other attribute, 
+			//1 format din doua atribute, 
+			//1 prefix substring, 
+			//1 matching substring, 
+			//1 direct child, 
+			//1 next sibling
+		
 		
 	}
+	
 
 }
---
+++ src/main/java/md/natalia/main/homework/Homework.java
@@ -0,0 +1,167 @@
+package md.natalia.main.homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		
+		//Tema pentru acasa Locator
+		
+		
+		 /**XPath Relative Locator*/
+		 
+	     //ChromeDriver driverChrome = new ChromeDriver();
+	     //driverChrome.get("https://www.olx.ro/");
+	     //driverChrome.manage().window().maximize();
+	     //driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+         //driverChrome.findElement(By.xpath("//span[@class='css-xu7uwr']")).click();
+		
+	     /**XPath Absolute Locator*/
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+       //driverChrome.findElement(By.xpath("/html/body/div/div/div/div/div/div/div/a/span")).click();		
+       
+       /**XPATH starts-with*/
+       /**tagname[starts-with(@Attribute, 'value']*/
+       
+       //ChromeDriver driverChrome = new ChromeDriver();
+		//driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+       //driverChrome.findElement(By.xpath("//a[starts-with(@span, 'css-xu')]")).click();
+       
+       
+        /**XPATH ends-with*/
+		/**tagname[ends-with(@Attribute, 'value']*/
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+       //driverChrome.get("https://www.olx.ro/");
+       //driverChrome.manage().window().maximize();
+       //driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+		//driverChrome.findElement(By.xpath("//a[ends-with(@span,'-xu7uwr')]")).click();		
+		
+		/**XPATH contains*/
+       /**tagname[contains(@Attribute, 'value']*/
+		
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+       //driverChrome.get("https://www.olx.ro/");
+       //driverChrome.manage().window().maximize();
+       //driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+       //driverChrome.findElement(By.xpath("//[contains(@span, '-xu7-')]")).click();
+       
+              
+		/**XPATH  text()*/
+		/**tagname[text()='actual text']*/
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+       //driverChrome.get("https://www.olx.ro/");
+       //driverChrome.manage().window().maximize();
+       //driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+	    //driverChrome.findElement(By.xpath("//a[text()='Inchiriere bunuri si vehicule']")).click();
+		
+	    
+	    /** XPath*/
+	    /** XPATH=//tagname[@Attribute=’Value’ and @Attribute=’Value’]*/
+	    
+	    //ChromeDriver driverChrome = new ChromeDriver();
+       //driverChrome.get("https://www.olx.ro/");
+       //driverChrome.manage().window().maximize();
+       //driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+	    //driverChrome.findElement(By.xpath("//a[@span='data cy'and @span='class']")).click();
+	    
+	    
+		/** XPath*/
+	    /** XPATH=//tagname[@Attribute=’Value’ or @Attribute=’Value’]*/
+	    
+	    //ChromeDriver driverChrome = new ChromeDriver();
+       //driverChrome.get("https://www.olx.ro/");
+       //driverChrome.manage().window().maximize();
+       //driverChrome.findElement(By.id("onetrust-accept-btn-handler")).click();
+	    //driverChrome.findElement(By.xpath("//a[@span='data cy'or @span='class']")).click();
+	
+		//Tema pentru acasa Locator CSS
+		//ID
+		
+			//ChromeDriver driverChrome = new ChromeDriver();
+			//driverChrome.get("https://www.olx.ro/");
+			//driverChrome.manage().window().maximize();
+			//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+							
+			
+			//Css Class
+			//tagName.Attribute_Value (valoarea clasei)
+			
+			//ChromeDriver driverChrome = new ChromeDriver();
+			//driverChrome.get("https://www.olx.ro/");
+			//driverChrome.manage().window().maximize();
+			//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+			//driverChrome.findElement(By.cssSelector("span.css-xu7uwr")).click();
+			//driverChrome.findElement(By.cssSelector("span.css-1o5oslh")).click();
+					
+			//4 Other attribute
+			//tagName[AttributeName=’AttributeValue’] formula generala
+			
+		   // ChromeDriver driverChrome = new ChromeDriver();
+		   // driverChrome.get("https://www.olx.ro/");
+			//driverChrome.manage().window().maximize();
+			//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+			//driverChrome.findElement(By.cssSelector("input[data-testid='search-input']")).click();
+			//driverChrome.findElement(By.cssSelector("button[class='css-1xla5xw']")).click();
+			//driverChrome.findElement(By.cssSelector("button[type='button']")).click();
+			
+			
+			//tagName[AttributeName=’AttributeValue’][ AttributeName=’AttributeValue’]
+			
+			
+		   //ChromeDriver driverChrome = new ChromeDriver();
+           //driverChrome.get("https://www.olx.ro/");
+	       //driverChrome.manage().window().maximize();
+	       //driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+	       //driverChrome.findElement(By.cssSelector("input[inputmode='numeric'][class='css-ygy1px']")).click();
+				
+			//1 prefix substring
+		
+			//ChromeDriver driverChrome = new ChromeDriver();
+			//driverChrome.get("https://www.olx.ro/");
+			//driverChrome.manage().window().maximize();
+			//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+			//driverChrome.findElement(By.cssSelector("span[class^='css-1ohf-']")).click();
+		
+					//1 matching substring
+		
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("li[data-cy*='-bar-']")).click();
+					
+		//1 direct child, 
+					
+		//ChromeDriver driverChrome = new ChromeDriver();
+	    //driverChrome.get("https://www.olx.ro/");
+		//driverChrome.manage().window().maximize();
+		//driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		//driverChrome.findElement(By.cssSelector("a[class='css-3o5qfb']>a[class='css-f0mzyq']")).click();
+		
+		//1 next sibling
+		
+		ChromeDriver driverChrome = new ChromeDriver();
+	    driverChrome.get("https://www.olx.ro/");
+		driverChrome.manage().window().maximize();
+		driverChrome.findElement(By.cssSelector("button[id='onetrust-accept-btn-handler']")).click();
+		driverChrome.findElement(By.cssSelector("div[class='css-1rwzo2t']>a[class='css-1gpccxq']")).click();
+	
+	
+			
+			
+			
+			
+			
+	}
+}
